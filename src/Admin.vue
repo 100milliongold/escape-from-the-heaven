96bfc3b0748379681bfd5e10a5731b3ee5545cee
xiilab <je.kim@xiilab.com>
2019-09-20 17:03:08 +0900

    init-data

---
+++ src/Admin.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="wrapper">
+        <NavVar/>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+          <!-- Main Content -->
+          <div id="content">
+            <Topbar/>
+            <router-view></router-view>
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import NavVar from '@/components/Admin/NavVar.vue'
+import Topbar from '@/components/Admin/Topbar.vue'
+import 'startbootstrap-sb-admin-2/css/sb-admin-2.css'
+
+export default {
+  components: {
+    NavVar,
+    Topbar,
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+// app.js
+import BootstrapVue from 'bootstrap-vue'
+import './custom.scss'
+
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss'
+import '@fortawesome/fontawesome-free/scss/solid.scss'
+import '@fortawesome/fontawesome-free/scss/brands.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#root')
+
+Vue.use(BootstrapVue)
